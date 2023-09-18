e15c948dfcea7985f5921bd4731da7fdcaeb0f4d
NowWeAre-Free <kontodarm@wp.pl>
2023-09-18 21:48:33 +0200

    :tada: init + button component test

---
+++ ecomm/src/App.js
@@ -0,0 +1,7 @@
+export function App() {
+  return <div className="App">123</div>;
+}
+
+export const func = () => {
+  console.log("s");
+};
---
+++ ecomm/src/button.jsx
@@ -0,0 +1,4 @@
+import { func } from "./App";
+export function Button() {
+  return <button onClick={func}>CLICK ME</button>;
+}
